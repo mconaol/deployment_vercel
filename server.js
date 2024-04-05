7a5a2fa5eca9ee077e315fa80d247514a7e115cd
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-04-04 19:36:10 -0700

    second commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require ('express')
 const app = express()
 require('dotenv').config()
-
+const PORT= process.env.PORT
 
 app.get('/', (req, res)=>{
     res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(PORT, ()=> console.log(`Server is running in port ${PORT}`))
 
-module.exports = app 
+module.exports = app ;
